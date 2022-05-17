720b4db962d0b0d3132ef0b49b04d3001f7e1b36
Akash singh <akash24524singh@gmail.com>
2022-05-17 15:29:55 +0530

    resulting data type

---
+++ CWH_10_resulting_data_type.java
@@ -0,0 +1,22 @@
+package com.CodeWithAkash;
+
+public class CWH_10_resulting_data_type {
+    public static void main(String[] args) {
+//    byte a = 4;
+//    short b = 2;
+//    float c = a+b;
+//    float d = 45.5f;
+//    int e = 33;
+//    float f = e + d;
+//     Increment and Decrement Operator
+        // i++ means first i will be assigned and then  i will be increment
+        // ++i means first i will be increment and then i will be assigned
+//        int i = 56;
+//        int b = i++ + 45;
+//        int c = i + 45;
+//        System.out.println(b);
+//        System.out.println(c);
+        char ch = 'y';
+        System.out.println(ch);
+    }
+}
